13a34ad700ab95f7efc240373945e26d7bfb249d
unknown <jcontreras39@gmail.com>
2020-05-09 13:11:48 -0500

    first push for practice

---
+++ client/src/components/DeleteBtn/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+// The ...props means, spread all of the passed props onto this element
+// That way we don't have to define them all individually
+function DeleteBtn(props) {
+  return (
+  //  ...props contact the onclick event
+   <span className="delete-btn" {...props} role="button" tabIndex="0">
+      ✗
+    </span>
+  );
+}
+
+export default DeleteBtn;
