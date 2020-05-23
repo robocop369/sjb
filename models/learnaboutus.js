f3f263e63be0ae5ff892c381de196fd1467ab16f
Stephanie Cedillo <shcedillo@gmail.com>
2020-05-22 22:00:41 -0500

    beginning to merge chicago api controllers and routes with mongodb

--- models/learnaboutus.js
+++ models/learnaboutus.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const learnaboutusSchema = new Schema({
-  aboutus_id: Schema.Types.ObjectId,
-  picture: { type: String, get: v => `${root}${v}` },
   firstname: { type: String, required: true },
+  picture: { type: String, get: v => `${root}${v}` },
   lastname: { type: String, required: true },
   quote: String,
-  contactinfo: [{ email: String, linkedin: String, instagram: String, twitter: String, facebook: String}]
+  email: String, 
+  linkedin: String, 
+  instagram: String, 
+  twitter: String, 
+  facebook: String
 });
 
 const LearnAboutUs = mongoose.model("LearnAboutUs", learnaboutusSchema);
