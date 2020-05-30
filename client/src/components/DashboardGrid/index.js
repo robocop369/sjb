992351e0259bde70e89950e61c382910dc8316df
Brandon Bayer <brandonabayer@gmail.com>
2020-05-30 11:04:50 -0500

    [client/src/components/DashboardGrid]

--- client/src/components/DashboardGrid/index.js
+++ client/src/components/DashboardGrid/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Container from "../components/Container";
-import Row from "../components/Row";
-import Col from "../components/Col";
+import Container from "../Container";
+import Row from "../Row";
+import Col from "../Col";
 
 // const DashboardGrid = (props) => {
     return (
