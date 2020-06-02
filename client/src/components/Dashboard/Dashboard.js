1aca4a9c686df199224f69d246f252a9ef9152fd
Brandon Bayer <brandonabayer@gmail.com>
2020-06-02 17:48:02 -0500

    cleaned up dashboard and console.logs

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -3,12 +3,6 @@ import Col from "react-bootstrap/Col";
 import ChicagoData from "./ChicagoData/ChicagoData";
 import {Row, Container } from "../Grid";
 import Accordion from "../Accordion";
-import AppFooter from "../AppFooter";
-import "../../../src/styles.css";
-
-
-// import { Footer } from "../components/Footer";
-
 function Dashboard() {
 
   return (
@@ -16,8 +10,6 @@ function Dashboard() {
     <div clasName="container dashboard">
       <Container>
         <Row>
-          <Col md={4}><button href="#portal" variant="primary">Map</button></Col>
-          <Col md={{ span: 4, offset: 4 }}>TESTING SPACE</Col>
           <br>
           </br>
         </Row>
@@ -25,9 +17,6 @@ function Dashboard() {
           <Col md={{ span: 5, offset: 4 }}>{<ChicagoData/>}</Col>
         </Row>
         <Accordion/>
-        <Row>
-        {/* <AppFooter/> */}
-        </Row>
       </Container>
     </div>
   );
